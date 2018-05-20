955d38ebe6d0e3b328cf7193497d3cf67c5458c9
RyanThiele <dynamensions@live.com>
2018-05-20 06:38:51 -0400

    Initial Check-in. Nothing but structure here.

--- Media Editor/MainWindow.xaml.cs
+++ Media Editor/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Xabe.FFmpeg;
+using Xabe.FFmpeg.Model;
 
 namespace Media_Editor
 {
@@ -20,9 +22,18 @@ namespace Media_Editor
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string output;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            IConversionResult result = await Conversion.ToMp4(@"D:\2.mp4", @"D:\3.mp4").Start();
+
         }
     }
 }
